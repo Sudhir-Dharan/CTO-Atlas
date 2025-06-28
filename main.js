b499fa5ffcadec5239c4068a02bd14c2caeb7ae2
sudhirdharan <sudhir.dharan@symphonytalent.com>
2025-06-28 17:28:44 +0530

    Split main sections into individual modules

--- main.js
+++ main.js
@@ -10,6 +10,15 @@ async function init() {
     loadModule('footer', 'modules/footer.html')
   ]);
 
+  await Promise.all([
+    loadModule('pillar1-placeholder', 'modules/pillar1.html'),
+    loadModule('pillar2-placeholder', 'modules/pillar2.html'),
+    loadModule('pillar3-placeholder', 'modules/pillar3.html'),
+    loadModule('pillar4-placeholder', 'modules/pillar4.html'),
+    loadModule('pillar5-placeholder', 'modules/pillar5.html'),
+    loadModule('interactive-placeholder', 'modules/interactive.html')
+  ]);
+
   const scriptText = await fetch(`modules/script.js?v=${Date.now()}`).then(r => r.text());
   const scriptEl = document.createElement('script');
   scriptEl.textContent = scriptText;
